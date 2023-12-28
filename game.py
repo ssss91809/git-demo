bb990f098935be67c37398caead86566fde34570
steven <ssss91809@gmail.com>
2023-12-28 18:45:45 +0800

    修正game

---
+++ game.py
@@ -0,0 +1,19 @@
+import random
+
+# 1~50
+# 使用者猜五次
+# 猜對提前離開
+# 猜錯，提示答案
+
+#題目
+
+x=random.randint(1,50)
+print(x)
+
+for i in range(5):
+    y=int(input('請猜數字(1~50):'))
+    if y==x:
+        print('猜對了!')
+        break
+    else:
+        print('猜錯了~~')
